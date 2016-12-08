307c6ff67a5ffec94380afba2b9dbc8dc0c85a1b
Daisuke <r111331@g.kobe-kosen.ac.jp>
2016-12-08 16:51:51 +0900

    [update] Output to RGB-color image

--- cnn.py
+++ cnn.py
@@ -7,7 +7,7 @@ IMAGE_H = 128
 IMAGE_W = 128
 IMAGE_SIZE = IMAGE_H*IMAGE_W
 
-IMAGE_KEYS = ['y_p1','y_p2' ,'y_p3' ,'y_dc0' ,'y_dc1', 'y_dc2', 'y_dc3', 'y_in']
+IMAGE_KEYS = ['y_p1','y_p2' ,'y_p3' ,'y_dc0' ,'y_dc1', 'y_dc2', 'y_dc3']
 
 W_RANGE = [128, 64, 32, 16]
 CH_RANGE = [3, 16, 32, 64]
@@ -24,17 +24,21 @@ for i in range(848):
     dep_img = cv2.imread(dep_dir, cv2.IMREAD_GRAYSCALE)
     col_img = cv2.resize(col_img, (IMAGE_H, IMAGE_W))
     dep_img = cv2.resize(dep_img, (IMAGE_H, IMAGE_W))
-    train_image.append(col_img.flatten().astype(np.float32)/255.0)
-    train_depth.append(dep_img.flatten().astype(np.float32)/255.0)
+    
+    dep_img = cv2.applyColorMap(dep_img, cv2.COLORMAP_JET)
+
+    train_image.append(col_img.flatten().astype(np.float32)/255.)
+    train_depth.append(dep_img.flatten().astype(np.float32)/255.)
 
 train_image = np.asarray(train_image)
 train_depth = np.asarray(train_depth)
 
+
 def b_n(input_):
-    shape = input_.get_shape().dims[-1].value
-    eps = 1e-12
-    gamma = tf.Variable(tf.ones([shape]))
-    beta = tf.Variable(tf.zeros([shape]))
+    shape = input_.get_shape().dims[3].value
+    eps = 1e-5
+    gamma = tf.Variable(tf.truncated_normal([shape], stddev=0.1))
+    beta = tf.Variable(tf.truncated_normal([shape], stddev=0.1))
     mean, variance = tf.nn.moments(input_, [0, 1, 2])
     return gamma * (input_ - mean) / tf.sqrt(variance + eps) + beta
 
@@ -48,11 +52,11 @@ def linear(input_, output_size, stddev=0.02):
     bias = tf.Variable(tf.constant(0.0, shape=[output_size]))
     return tf.matmul(input_, matrix) + bias
 
-def conv(image, out_dim, name, c=5, k=1, stddev=0.02, wd=0.001):
+def conv(image, out_dim, name, c=5, k=1, stddev=0.02, wd=0.0001):
     with tf.name_scope(name) as scope:
         W = tf.Variable(tf.truncated_normal([c, c, image.get_shape().dims[-1].value, out_dim], stddev=stddev))
         b = tf.Variable(tf.constant(0.0, shape=[out_dim]))
-        y = tf.nn.conv2d(image, W, strides=[1, k, k, 1], padding='SAME')+b
+        y = tf.nn.conv2d(image, W, strides=[1, k, k, 1], padding='SAME') + b
         if wd:
             weight_decay = tf.mul(tf.nn.l2_loss(W), wd, name='weight_loss')
             tf.add_to_collection('w_loss', weight_decay)
@@ -61,20 +65,22 @@ def conv(image, out_dim, name, c=5, k=1, stddev=0.02, wd=0.001):
 def pool(x, k=2):
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-def deconv(image, output_shape, name, c=5, k=2, stddev=0.02, wd=0.001):
+def deconv(image, output_shape, name, c=5, k=2, stddev=0.02, wd=0.0001):
     with tf.name_scope(name) as scope:
-        W = tf.Variable(tf.truncated_normal([c, c, output_shape[-1], image.get_shape().dims[-1].value], stddev=0.02))    
+        W = tf.Variable(tf.truncated_normal([c, c, output_shape[-1], image.get_shape().dims[-1].value], stddev=stddev))    
         b = tf.Variable(tf.constant(0.0, shape=[output_shape[-1]]))
         y = tf.nn.deconv2d(image, W, output_shape=output_shape, strides=[1,k,k,1], padding='SAME') + b
         if wd:
             weight_decay = tf.mul(tf.nn.l2_loss(W), wd, name='weight_loss')
             tf.add_to_collection('w_loss', weight_decay)
-        return y
+        return b_n(y)
 
 def discriminator(image, depth):
     dim = 12
     with tf.name_scope('disc') as scope:
         image = tf.reshape(image, [-1, IMAGE_H, IMAGE_W, 3])
+        depth = tf.reshape(depth, [-1, IMAGE_H, IMAGE_W, 3])
+
         h0 = lrelu(conv(image, dim, k=2, name='h0_conv'))
         h1 = lrelu(conv(h0, dim*2, k=2, name='h1_conv'))
 
@@ -89,32 +95,30 @@ def discriminator(image, depth):
 
 def inference(input_):
     with tf.name_scope('conv') as scope:
-        wd = 0.0
         dim = 16
         input_ = tf.reshape(input_, [BAT_SIZE, IMAGE_H, IMAGE_W, 3])
         #convolutional layers
-        y_c0 = tf.nn.relu(conv(input_, dim, name='c0', wd=wd))
+        y_c0 = tf.nn.relu(conv(input_, dim, name='c0'))
         y_p0 = pool(y_c0)
-        y_c1 = tf.nn.relu(conv(y_p0, dim * 2, name='c1', wd=wd))
-        y_c2 = tf.nn.relu(conv(y_c1, dim * 2, name='c2', wd=wd))
+        y_c1 = tf.nn.relu(conv(y_p0, dim * 2, name='c1'))
+        y_c2 = tf.nn.relu(conv(y_c1, dim * 2, name='c2'))
         y_p2 = pool(y_c2)
-        y_c3 = tf.nn.relu(conv(y_p2, dim * 4, name='c3', wd=wd))
+        y_c3 = tf.nn.relu(conv(y_p2, dim * 4, name='c3'))
         y_p3 = pool(y_c3)
 
     with tf.name_scope('gen') as scope:
         #generator
-        y_dc0 = tf.nn.relu(deconv(y_p3, [BAT_SIZE, W_RANGE[2], W_RANGE[2], dim * 2], c=3, name='dc0')))
-        y_dc1 = tf.nn.relu(deconv(y_dc0, [BAT_SIZE, W_RANGE[2], W_RANGE[2], dim], c=5, k=1, name='dc1')))
-        y_dc2 = tf.nn.relu(deconv(y_dc1, [BAT_SIZE, W_RANGE[1], W_RANGE[1], dim], c=5, name='dc2')))
-        y_in = b_n(y_dc2 + y_p0)
-        y_dc3 = tf.nn.sigmoid(deconv(y_in, [BAT_SIZE, W_RANGE[0], W_RANGE[0], 1], c=5, name='dc3'))
+        y_dc0 = tf.nn.relu(deconv(y_p3, [BAT_SIZE, W_RANGE[2], W_RANGE[2], dim * 2], c=3, name='dc0'))
+        y_dc1 = tf.nn.relu(deconv(y_dc0, [BAT_SIZE, W_RANGE[1], W_RANGE[1], dim], c=5, name='dc2'))
+        y_dc2 = tf.nn.relu(deconv(tf.concat(3, [y_dc1, y_p0]), [BAT_SIZE, W_RANGE[1], W_RANGE[1], dim], k=1, name='dcin'))
+        y_dc3 = tf.nn.sigmoid(deconv(y_dc2, [BAT_SIZE, W_RANGE[0], W_RANGE[0], 3], c=5, name='dc3'))
         
-        return {'y_p0':y_p0, 'y_p2':y_p2, 'y_p3':y_p3, 'y_dc0':y_dc0, 'y_dc1':y_dc1, 'y_in':y_in, 'y_dc2':y_dc2, 'y_dc3':y_dc3}
+        return {'y_p0':y_p0, 'y_p2':y_p2, 'y_p3':y_p3, 'y_dc0':y_dc0, 'y_dc1':y_dc1, 'y_dc2':y_dc2, 'y_dc3':y_dc3}
 
 
 def loss(y, y_):
     with tf.name_scope('loss') as scope:
-        loss = tf.nn.l2_loss(y - y_) + tf.add_n(tf.get_collection('w_loss'))
+        loss = tf.nn.l2_loss(y - y_)
         tf.scalar_summary("loss", loss)
     return loss
         
@@ -122,13 +126,13 @@ def loss(y, y_):
 def gen_loss(h, h_):
     with tf.name_scope('g_loss') as scope:
         zero_h = tf.zeros_like(h_)
-        g_entropy = tf.reduce_mean(- (zero_h * tf.log(h + 1e-12) + (1. - zero_h) * tf.log(1. - h + 1e-12)))
+        g_entropy = tf.reduce_mean(- (zero_h * tf.log(h + 1e-7) + (1. - zero_h) * tf.log(1. - h + 1e-7)))
         tf.scalar_summary("g_entropy", g_entropy)
     return g_entropy
 
 def disc_loss(h, h_):
     with tf.name_scope('d_loss') as scope:
-        d_entropy = tf.reduce_mean(- (h_ * tf.log(h + 1e-12) + (1. - h_) * tf.log(1. - h + 1e-12)))
+        d_entropy = tf.reduce_mean(- (h_ * tf.log(h + 1e-7) + (1. - h_) * tf.log(1. - h + 1e-7)))
         tf.scalar_summary("d_entropy", d_entropy)
         correct_prediction = tf.reduce_mean(h_ * h + (1. - h_)*(1. - h))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -138,19 +142,19 @@ def train(loss):
     with tf.name_scope('train') as scope:
         c_vars = tf.get_collection(tf.GraphKeys.VARIABLES, scope='conv')
         g_vars = tf.get_collection(tf.GraphKeys.VARIABLES, scope='gen')
-        train_step = tf.train.AdamOptimizer(3e-4).minimize(loss, var_list=list(c_vars + g_vars))
+        train_step = tf.train.AdamOptimizer(2e-5).minimize(loss, var_list=list(c_vars + g_vars))
     return train_step
 
 def g_train(g_loss):
     with tf.name_scope('g_train') as scope:
         g_vars = tf.get_collection(tf.GraphKeys.VARIABLES, scope='gen')
-        train_step = tf.train.AdamOptimizer(3e-4).minimize(g_loss, var_list=g_vars)
+        train_step = tf.train.AdamOptimizer(2e-5).minimize(g_loss, var_list=g_vars)
     return train_step
 
 def d_train(d_loss):
     with tf.name_scope('d_train') as scope:
         d_vars = tf.get_collection(tf.GraphKeys.VARIABLES, scope='disc')
-        train_step = tf.train.AdamOptimizer(3e-4).minimize(d_loss, var_list=d_vars)
+        train_step = tf.train.AdamOptimizer(2e-5).minimize(d_loss, var_list=d_vars)
     return train_step
 
 def gen_image(result):
@@ -158,12 +162,18 @@ def gen_image(result):
     for y in result.values():
         size = y.get_shape().dims[1].value
         ch = y.get_shape().dims[3].value
-
         row = col = np.trunc(np.sqrt(BAT_SIZE)).astype(np.int32)
-        images = tf.cast(tf.mul(y,255.), tf.uint8)
-        images = [tf.squeeze(image, [3]) for image in tf.split(3, ch, images)]
-        images = tf.reshape(images[0], [BAT_SIZE, size, size, 1])
-        images = [tf.squeeze(image, [0]) for image in tf.split(0, BAT_SIZE, images)]
+        images = tf.cast(tf.mul(y, 255.), tf.uint8)
+        if ch == 3:
+            images = [tf.expand_dims(tf.squeeze(image, [3]), 3) for image in tf.split(3, 3, images)]
+            images = tf.concat(3, [images[2], images[1], images[0]])
+            images = tf.reshape(images, [BAT_SIZE, size, size, 3])
+            images = [tf.squeeze(image, [0]) for image in tf.split(0, BAT_SIZE, images)]
+        else:
+            images = [tf.squeeze(image, [3]) for image in tf.split(3, ch, images)]
+            images = tf.reshape(images[0], [BAT_SIZE, size, size, 1])
+            images = [tf.squeeze(image, [0]) for image in tf.split(0, BAT_SIZE, images)]
+        
         rows = []
         for i in range(row):
             rows.append(tf.concat(1,images[col * i + 0:col * i + col]))
@@ -175,9 +185,10 @@ def gen_image(result):
 with tf.Graph().as_default():
 
     x = tf.placeholder("float", [None, IMAGE_SIZE * 3], name="x")
-    y_ = tf.placeholder("float", [None, IMAGE_SIZE], name="y_")
+    y_ = tf.placeholder("float", [None, IMAGE_SIZE * 3], name="y_")
 
-    y_ph = tf.reshape(y_, [BAT_SIZE, IMAGE_H, IMAGE_W, 1])
+    x_ph = tf.reshape(x, [BAT_SIZE, IMAGE_H, IMAGE_W, 3])
+    y_ph = tf.reshape(y_, [BAT_SIZE, IMAGE_H, IMAGE_W, 3])
 
     result = inference(x)
     y_out = result['y_dc3']
@@ -204,7 +215,7 @@ with tf.Graph().as_default():
     d_loss, acc = disc_loss(h, h_)
     g_loss = gen_loss(h, h_)
 
-    train_op = train(loss) 
+    train_op = train(loss + tf.add_n(tf.get_collection('w_loss')) )
     d_train_op = d_train(d_loss)
     g_train_op = g_train(g_loss)
 
@@ -212,6 +223,7 @@ with tf.Graph().as_default():
     summary_op = tf.merge_all_summaries()
     
     images = gen_image(result)
+    in_image = gen_image(dict(zip(range(0,9), [x_ph])))
     res_image = gen_image(dict(zip(range(0,9), [y_ph])))
 
     init = tf.initialize_all_variables()
@@ -232,7 +244,7 @@ with tf.Graph().as_default():
             for i in range(len(train_image)/batch_size):
                 if train_flag == 0:
                     train_res = sess.run(train_op, feed_dict = train_feed)
-                    if step > 8000:
+                    if step > 15000:
                         print "----------------* cnn_train finished *----------------"
                         print("")
                         train_flag = 1
@@ -240,6 +252,14 @@ with tf.Graph().as_default():
                     train_res = sess.run([g_train_op, acc], feed_dict = train_feed)
                     if train_res[1] < 0.8:
                         sess.run(d_train_op, feed_dict = train_feed)
+
+            if step == 0:
+                in_img = sess.run(in_image, {x: train_image[:10]})
+                target = sess.run(res_image, {y_: train_depth[:10]})
+                with open("database/image/input.png", 'wb') as f:
+                    f.write(in_img[0])
+                with open("database/image/target.png", 'wb') as f:
+                    f.write(target[0])
 
             if step % 10 == 0:
                 # output results
@@ -256,8 +276,8 @@ with tf.Graph().as_default():
                 summary_str = sess.run(summary_op,{x: train_image[:10], y_:train_depth[:10]})
                 summary_writer.add_summary(summary_str,step)
             
-            if step % 100 == 0:
-                num = step/100
+            if step % 10 == 0:
+                num = step/10
                 val = sess.run(images, {x: train_image[:10], y_: train_depth[:batch_size]})
                 for j in range(len(val)):
                     with open("database/image/result%s-%03d.png" % (IMAGE_KEYS[j], num), 'wb') as f:
