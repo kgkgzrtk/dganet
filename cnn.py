3b54f42f4a390e8119f58b2f5dcb58cc88dcb905
Daisuke <r111331@g.kobe-kosen.ac.jp>
2016-12-22 16:15:27 +0900

    [Change] connecting

--- cnn.py
+++ cnn.py
@@ -7,7 +7,7 @@ IMAGE_H = 128
 IMAGE_W = 128
 IMAGE_SIZE = IMAGE_H*IMAGE_W
 
-IMAGE_KEYS = ['y_p0','y_p3' ,'y_p4' ,'y_dc2' ,'y_dc5', 'y_dc6']
+IMAGE_KEYS = ['y_p1','y_p4' ,'y_p5' ,'y_dc2' ,'y_dc5', 'y_dc6']
 
 W_RANGE = [128, 64, 32, 16, 8, 4]
 CH_RANGE = [3, 16, 32, 64, 128, 256]
@@ -101,25 +101,26 @@ def inference(input_):
         #convolutional layers
         
         y_c0 = tf.nn.relu(conv(input_, dim, c=5, name='c0'))
-        y_p0 = pool(y_c0)
-        y_c1 = tf.nn.relu(conv(y_p0, dim * 2, c=4, name='c1'))
-        y_c2 = tf.nn.relu(conv(y_c1, dim * 4, k=2, name='c2'))
-        y_c3 = tf.nn.relu(conv(y_c2, dim * 8, k=2, name='c3'))
-        y_p3 = pool(y_c3)
-        y_c4 = tf.nn.relu(conv(y_p3, dim * 16, name='c4'))
+        y_c1 = tf.nn.relu(conv(y_c0, dim * 2, c=4, name='c1'))
+        y_p1 = pool(y_c1)
+        y_c2 = tf.nn.relu(conv(y_p1, dim * 2, k=2, name='c2'))
+        y_c3 = tf.nn.relu(conv(y_c2, dim * 4, c=4, name='c3'))
+        y_c4 = tf.nn.relu(conv(y_c3, dim * 8, k=2, name='c4'))
         y_p4 = pool(y_c4)
+        y_c5 = tf.nn.relu(conv(y_p4, dim * 16, name='c5'))
+        y_p5 = pool(y_c5)
 
     with tf.name_scope('gen') as scope:
         #generator
-        y_dc0 = tf.nn.relu(b_n(deconv(y_p4, [BAT_SIZE, W_RANGE[4], W_RANGE[4], dim * 8], c=3, name='dc0')))
+        y_dc0 = tf.nn.relu(b_n(deconv(y_p5, [BAT_SIZE, W_RANGE[4], W_RANGE[4], dim * 8], c=3, name='dc0')))
         y_dc1 = tf.nn.relu(b_n(deconv(y_dc0, [BAT_SIZE, W_RANGE[3], W_RANGE[3], dim * 4], c=3, name='dc1')))
         y_dc2 = tf.nn.relu(b_n(deconv(y_dc1, [BAT_SIZE, W_RANGE[2], W_RANGE[2], dim * 2], c=4, name='dc2')))
-        y_dc3 = tf.nn.relu(b_n(deconv(y_dc2, [BAT_SIZE, W_RANGE[2], W_RANGE[2], dim], c=3, k=1, name='dc3')))
-        y_dc4 = tf.nn.relu(b_n(deconv(y_dc3, [BAT_SIZE, W_RANGE[1], W_RANGE[1], dim], c=3, name='dc4')))
-        y_dc5 = tf.nn.relu(b_n(deconv(y_dc4 + y_p0, [BAT_SIZE, W_RANGE[1], W_RANGE[1], dim], c=4, k=1, name='dc5')))
-        y_dc6 = tf.nn.sigmoid(deconv(y_dc5, [BAT_SIZE, W_RANGE[0], W_RANGE[0], 1], c=5, name='dc6'))
+        y_dc3 = tf.nn.relu(b_n(deconv(y_dc2, [BAT_SIZE, W_RANGE[1], W_RANGE[1], dim * 2], c=3, name='dc3')))
+        y_dc4 = tf.nn.relu(b_n(deconv(y_dc3, [BAT_SIZE, W_RANGE[0], W_RANGE[0], dim], c=3, name='dc4')))
+        y_dc5 = tf.nn.relu(b_n(deconv(y_dc4 + y_c0, [BAT_SIZE, W_RANGE[0], W_RANGE[0], dim], c=4, k=1, name='dc5')))
+        y_dc6 = tf.nn.sigmoid(deconv(y_dc5, [BAT_SIZE, W_RANGE[0], W_RANGE[0], 1], c=5, k=1, name='dc6'))
         
-    y = [y_p0 ,y_p3, y_p4, y_dc2, y_dc5, y_dc6]
+    y = [y_p1 ,y_p4, y_p5, y_dc2, y_dc5, y_dc6]
     return dict(zip(IMAGE_KEYS, y))
 
 
