1b60ff3e043f970bf2d529aa4fcae5161953410d
Daisuke <r111331@g.kobe-kosen.ac.jp>
2017-01-24 18:40:28 +0900

    [update] faster printing

--- 3d.py
+++ 3d.py
@@ -1,133 +1,186 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+from ctypes import *
 
 import sys
 import cv2
 import numpy as np
+import Image
+
 
 IMAGE_W = 640
 IMAGE_H = 480
 
-area = 4
+area = 8
+biasZ = 100
 
-dwnX = 0
-dwnY = 0
+buf = None
+tex_id = None
+
+I_MAX = IMAGE_W/area -1 -1
+J_MAX = IMAGE_H/area -1 -1
+
+
+movX = 0
+movY = 0
+movZ = -500
 angX = 0
 angY = 0
 movef = GL_FALSE
+vertex = []
+texture = []
 
-t_ = [320.0, 240.0, 300.0]
-v_ = [640.0 + 400.0, 480.0 + 200.0, 1200.0]
 
-col_dir = "database/display/" + "image" + ".png"
-dep_dir = "database/display/" + "depth" + ".png"
-col_img = cv2.imread(col_dir)
+dep_dir = "database/display/" + "depth757" + ".png"
 dep_img = cv2.imread(dep_dir, cv2.IMREAD_GRAYSCALE)
-col_img = cv2.resize(col_img, (IMAGE_W, IMAGE_H))
 dep_img = cv2.resize(dep_img, (IMAGE_W, IMAGE_H))
-
-col_img = col_img.astype(np.float32)/255.0
 dep_img = (150.0 - dep_img.astype(np.float32))*3.0
 
-def degree(r):
-    return r * 180. / np.pi
+col_dir = "database/display/" + "image" + ".png"
+pcol = Image.open(col_dir)
+pcol_img = np.array(list(pcol.getdata()),np.int8)
 
-def putup(i,j):
-    # draw white polygon
-    glColor3f(col_img[j][i][2],col_img[j][i][1] , col_img[j][i][0])
-    glVertex3f(i, IMAGE_H-j, dep_img[j][i])
-
-    glColor3f(col_img[j+area][i][2],col_img[j+area][i][1] , col_img[j+area][i][0])
-    glVertex3f(i, IMAGE_H-(j+area), dep_img[j+area][i])
-
-    glColor3f(col_img[j+area][i+area][2],col_img[j+area][i+area][1] , col_img[j+area][i+area][0])
-    glVertex3f(i+area, IMAGE_H-(j+area), dep_img[j+area][i+area])
-
-    glColor3f(col_img[j][i+area][2],col_img[j][i+area][1] , col_img[j][i+area][0])
-    glVertex3f(i+area, IMAGE_H-j, dep_img[j][i+area])
 
 def display():
-    global angX, angY
+
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-    glPushMatrix()
 
-    rotateX = np.cos(angX) * np.cos(angY)
-    rotateY = np.sin(angY)
-    rotateZ = np.sin(angX) * np.cos(angY)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glTranslatef(movX,movY,movZ)
+    glRotate(angX, 1., 0 ,0)
+    glRotate(angY, 0, 1., 0)
+    print("rotated %f %f"%(angX, angY))
+
+    glEnableClientState(GL_VERTEX_ARRAY)
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY)
     
-    glTranslatef(t_[0],t_[1],t_[2])
-    glRotate(degree(rotateX), 1, 0 ,0)
-    glRotate(degree(rotateY), 0, 1, 0)
-    glRotate(degree(rotateZ), 0, 0, 1)
-    glTranslatef(-t_[0],-t_[1],-t_[2])
+    glBindBuffer(GL_ARRAY_BUFFER, buf[0])
+    glTexCoordPointer(2, GL_FLOAT, 0, None)
 
-    print("rotated %f %f %f" % (rotateX,rotateY, rotateZ))
+    glBindBuffer(GL_ARRAY_BUFFER, buf[1])
+    glVertexPointer(3, GL_FLOAT, 0, None)
 
-    glBegin(GL_QUADS)
-    for i in range(IMAGE_W/area - 1 - 1):
-        for j in range(IMAGE_H/area - 1 - 1):
-            putup(i*area,j*area)
-    glEnd()
-    glFlush()
-    glPopMatrix()
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D,tex_id)
+    glDrawArrays(GL_QUADS, 0, len(vertex))
+    glDisable(GL_TEXTURE_2D)
+
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY)
+    glDisableClientState(GL_VERTEX_ARRAY)
+    glutSwapBuffers()
+
+def spin_key(key, x, y):
+    global angX, angY
+    if key == GLUT_KEY_LEFT:
+        angY -= 5.
+    if key == GLUT_KEY_RIGHT:
+        angY += 5.
+    if key== GLUT_KEY_UP:
+        angX -= 5.
+    if key== GLUT_KEY_DOWN:
+        angX += 5.
+    glutPostRedisplay()
 
 
-def mouse(button, state, x, y):
-    global dwnX, dwnY, movef
-    if button == GLUT_LEFT_BUTTON & state == GLUT_DOWN:
-        print "click!"
-        dwnX = x/32.
-        dwnY = y/32.
-        movef = GL_TRUE
-    if button == GLUT_LEFT_BUTTON & state == GLUT_UP:
-        movef = GL_FALSE
+def keyboard(key, x, y):
+    global movX, movY, movZ
+    if key == 'a':
+        movX += 5.
+    if key == 'd':
+        movX -= 5.
+    if key == 'w':
+        movZ += 5.
+    if key == 's':
+        movZ -= 5.
+    if key == 'r':
+        movY += 5.
+    if key == 'f':
+        movY -= 5.
+    glutPostRedisplay()
 
-def drag(x,y):
-    global angX, angY, dwnX, dwnY, movef
-    if movef == GL_TRUE:
-        angX += x/32. - dwnX
-        angY += y/32. - dwnY
-        dwnX = x/32.
-        dwnY = y/32.
-        glutPostRedisplay()
+def init_vertex(ver, tex):
 
+    entX = float(area)/float(IMAGE_W)
+    entY = float(area)/float(IMAGE_H)
+    for _i in range(I_MAX):
+        for _j in range(J_MAX):
+            i=area*_i
+            j=area*_j
+            texX = float(i)/float(IMAGE_W)
+            texY = float(j)/float(IMAGE_H)
+
+            ver.extend([i-IMAGE_W/2, IMAGE_H-j-IMAGE_H/2, dep_img[j][i]-biasZ])
+            ver.extend([i-IMAGE_W/2, IMAGE_H-(j+area)-IMAGE_H/2, dep_img[j+area][i]-biasZ])
+            ver.extend([i+area-IMAGE_W/2, IMAGE_H-(j+area)-IMAGE_H/2, dep_img[j+area][i+area]-biasZ])
+            ver.extend([i+area-IMAGE_W/2, IMAGE_H-j-IMAGE_H/2, dep_img[j][i+area]-biasZ])
+
+            tex.extend([texX,texY])
+            tex.extend([texX,texY+entY])
+            tex.extend([texX+entX,texY+entY])
+            tex.extend([texX+entX,texY])
+
+def init_texture():
+    global tex_id
+    glPixelStorei(GL_UNPACK_ALIGNMENT,1)
+    tex_id = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D,tex_id)
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S, GL_CLAMP)
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_CLAMP)
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                    IMAGE_W,
+                    IMAGE_H, 0,
+                    GL_RGB,GL_UNSIGNED_BYTE,
+                    pcol_img)
+    glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE, GL_DECAL)
+    glEnable(GL_AUTO_NORMAL)
+    glEnable(GL_DEPTH_TEST)
+    glFrontFace(GL_CCW)
+    glEnable(GL_CULL_FACE)
+    glCullFace(GL_BACK)
 
 def init():
+    global buf
     # select clearing color
     glClearColor(1.0, 1.0, 1.0, 0.0)
     
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
-    glFrontFace(GL_CCW)
-    glEnable(GL_CULL_FACE)
-    glCullFace(GL_BACK)
+    buf = glGenBuffers(2)
+    glBindBuffer(GL_ARRAY_BUFFER,buf[0])
+    glBufferData(GL_ARRAY_BUFFER, len(texture)*4, (ctypes.c_float*len(texture))(*texture), GL_STATIC_DRAW)
+    glBindBuffer(GL_ARRAY_BUFFER,buf[1])
+    glBufferData(GL_ARRAY_BUFFER, len(vertex)*4, (ctypes.c_float*len(vertex))(*vertex), GL_STATIC_DRAW)
+
 
     # initialize viewing values
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(30.0, IMAGE_W/IMAGE_H, 0.1, 10000.0)
+    gluPerspective(450.0, IMAGE_W/IMAGE_H, 0.1, 10000.0)
 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    #v_ = [20.0, 20.0, 500.0]
-    #t_ = [10.0, 10.0, 0.0]
-    gluLookAt(v_[0], v_[1], v_[2], t_[0], t_[1], t_[2] , 0.0, 1.0, 0.0)
-
-    #glOrtho(0, IMAGE_W, IMAGE_H, 0, 0, 255)
+    glEnable(GL_DEPTH_TEST)
 
 
 def main():
     glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB)
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_REPEAT)
     glutInitWindowSize(IMAGE_W, IMAGE_H)
     glutInitWindowPosition(100, 100)
     glutCreateWindow("3DVIEW")
+    init_vertex(vertex,texture)
     init()
+    init_texture()
+    glutSpecialFunc(spin_key)
+    glutKeyboardFunc(keyboard)
     glutDisplayFunc(display)
-    glutMouseFunc(mouse)
-    glutMotionFunc(drag)
-    glutSwapBuffers()
+    print("Mainloop")
     glutMainLoop()
 
 if __name__ == "__main__":
